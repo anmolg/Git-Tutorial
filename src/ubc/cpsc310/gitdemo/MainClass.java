46a5081b2aabc183bea611e435b30f873d12cc92
Anmol <Anmol@128.189.192.112>
2012-09-10 14:05:50 -0700

    Initial Commit

---
+++ src/ubc/cpsc310/gitdemo/MainClass.java
@@ -0,0 +1,10 @@
+package ubc.cpsc310.gitdemo;
+
+public class MainClass {
+	
+	public static void main(String[] args) {
+		System.out.println("Trying out GIT");
+		System.out.println("Dah");
+	}
+
+}
